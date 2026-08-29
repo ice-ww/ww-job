a40e9e5f09bda8ff1b89fce6666cf9ab16290b35
ice-ww <643418356@qq.com>
2026-08-29 10:35:04 +0800

    feat: RegistryController 补 GET /registry/list 查询在线执行器

--- ww-job-admin/src/main/java/com/wwjob/admin/controller/RegistryController.java
+++ ww-job-admin/src/main/java/com/wwjob/admin/controller/RegistryController.java
@@ -1,11 +1,17 @@
 package com.wwjob.admin.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.wwjob.admin.entity.JobRegistry;
+import com.wwjob.admin.mapper.JobRegistryMapper;
 import com.wwjob.admin.service.RegistryService;
 import com.wwjob.core.model.RegistryParam;
 import com.wwjob.core.model.ReturnT;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author 王威
@@ -14,11 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegistryController {
     private final RegistryService registryService;
-    public RegistryController(RegistryService registryService) { this.registryService = registryService; }
+    private final JobRegistryMapper registryMapper;
+    public RegistryController(RegistryService registryService, JobRegistryMapper registryMapper) {
+        this.registryService = registryService;
+        this.registryMapper = registryMapper;
+    }
 
     @PostMapping("/registry")
     public ReturnT<String> registry(@RequestBody RegistryParam param) {
         return registryService.registry(param);
+    }
+
+    @GetMapping("/registry/list")
+    public List<JobRegistry> list() {
+        return registryMapper.selectList(new QueryWrapper<JobRegistry>()
+                .orderByAsc("job_group_id"));
     }
 
     @PostMapping("/heartbeat")
